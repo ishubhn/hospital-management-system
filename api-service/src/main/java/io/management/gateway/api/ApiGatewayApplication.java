3ae9f2bdf02931758bff7e3dc8f348410440353c
Shubham Nagre <shubhamnagre@gmail.com>
2023-03-04 22:13:19 +0530

    [UPDATE] Changed API gateway application name

--- api-service/src/main/java/io/management/gateway/api/ApiGatewayApplication.java
+++ api-service/src/main/java/io/management/gateway/api/ApiGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ApiApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
